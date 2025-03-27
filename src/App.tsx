a669b6e902211cea47a1e0a63b1704552cadc412
nkemdilim <Julie>
2025-03-28 00:54:35 +0100

    List Group

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Message from './message';
+import ListGroup from './components/ListGroup';
 function App() {
   // return <div><Message></Message></div>; OR, shorter ---
-  return <div><Message /></div>;
+  return <div><ListGroup /></div>;
 }
 
 export default App; // export this component so that it can be used
